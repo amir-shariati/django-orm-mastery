d2022158dc54b4209793999204715892fb99b75c
amirshariati <seyed.amir.shariati@gmail.com>
2024-01-04 17:54:39 +0330

    add Brand model

--- inventory/models.py
+++ inventory/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     qty = models.IntegerField()
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)

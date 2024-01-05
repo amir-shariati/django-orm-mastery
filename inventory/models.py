f4bc0912c9798f2c2cedcd04c5bcb3a8996beb01
amirshariati <seyed.amir.shariati@gmail.com>
2024-01-05 19:23:28 +0330

    add __str__ to Product model

--- inventory/models.py
+++ inventory/models.py
@@ -5,6 +5,9 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     qty = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)

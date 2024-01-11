4751ce24595f6312092e6e02629c64b9cb96f44b
amirshariati <seyed.amir.shariati@gmail.com>
2024-01-11 14:11:06 +0330

    add verbose name to brand name

--- inventory/models.py
+++ inventory/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='Brand name')
 
     def __str__(self):
         return self.name
